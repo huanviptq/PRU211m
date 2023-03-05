36846ccd3ebb57f7410dc6087b59d1373c7ef19a
huanviptq <huanviptq46@gmail.com>
2023-03-05 17:50:13 +0700

    add menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -33,6 +34,7 @@ public class PauseMenu : MonoBehaviour
     }
 
     public void LoadMenu(){
-        Debug.Log("Loading...");
+        SceneManager.LoadScene("Main Menu");
+        Resume();
     }
 }
