0b44eb496374325089441c42fee8031469cdeabc
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2023-03-21 23:45:31 +0700

    update something

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,26 +5,86 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /// <summary>
+    /// run speed of the player
+    /// </summary>
     [SerializeField] float runSpeed = 5f;
+    /// <summary>
+    /// jump power of the player
+    /// </summary>
     [SerializeField] float jumpPower = 5f;
+    /// <summary>
+    /// collider to check if the player is on the ground
+    /// </summary>
     [SerializeField] Transform groundCheckCollider;
+    /// <summary>
+    /// point to check if the player is in range of an enemy
+    /// </summary>
     [SerializeField] Transform attackPoint;
+    /// <summary>
+    /// range of the attack (how far the player can attack)
+    /// </summary>
     [SerializeField] float attackRange = 0.5f;
+    /// <summary>
+    /// rate of the attack (how many times per second)
+    /// </summary>
     [SerializeField] float attackRate = 2f;
+    /// <summary>
+    /// damage of the attack (how much damage the player does)
+    /// </summary>
     [SerializeField] int attackDamage = 50;
+    /// <summary>
+    /// damage of hazard to player
+    /// </summary>
     [SerializeField] int hazardDamage = 20;
+    /// <summary>
+    /// slash prefab to spawn
+    /// </summary>
     [SerializeField] GameObject slash;
+    /// <summary>
+    /// slash point to spawn slash prefab
+    /// </summary>
     [SerializeField] Transform slashPoint;
+    /// <summary>
+    /// time until the next attack
+    /// </summary>
     float nextAttackTime = 0f;
+    /// <summary>
+    /// audio player
+    /// </summary>
     AudioPlayer audioPlayer;
+    /// <summary>
+    /// radius of the ground check collider
+    /// </summary>
     const float groundCheckRadius = 0.2f;
+    /// <summary>
+    /// input of the player
+    /// </summary>
     Vector2 moveInput;
+    /// <summary>
+    /// rigidbody of the player
+    /// </summary>
     Rigidbody2D rb;
+    /// <summary>
+    /// animator of the player
+    /// </summary>
     Animator animator;
+    /// <summary>
+    /// capsule collider of the player
+    /// </summary>
     CapsuleCollider2D capsule;
+    /// <summary>
+    /// box collider of the player
+    /// </summary>
     BoxCollider2D box;
+    /// <summary>
+    /// if the player is on the ground
+    /// </summary>
     bool isGrounded = false;
 
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -34,15 +94,22 @@ public class PlayerMovement : MonoBehaviour
         audioPlayer = FindObjectOfType<AudioPlayer>();
     }
 
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
         Run();
         FlipSprite();
-        animator.SetFloat("yVelocity", rb.velocity.y);
+        animator.SetFloat("yVelocity", rb.velocity.y); 
         GroundCheck();
         DieByHazard();
     }
 
+    /// <summary>
+    /// Move the player
+    /// </summary>
+    /// <param name="value"></param>
     void OnMove(InputValue value)
     {
         if(!PauseMenu.isPaused){
@@ -50,6 +117,10 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    ///  Jump the player
+    /// </summary>
+    /// <param name="value"></param>
     void OnJump(InputValue value){
         if(!box.IsTouchingLayers(LayerMask.GetMask("Ground"))){
             return;
@@ -61,6 +132,9 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Attack the player
+    /// </summary>
     void OnAttack(){
         if(Time.time >= nextAttackTime && !PauseMenu.isPaused){
             animator.SetTrigger("Attack");
@@ -74,6 +148,9 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// OnDrawGizmosSelected
+    /// </summary>
     void OnDrawGizmosSelected() {
         if(attackPoint == null){
             return;
@@ -81,6 +158,9 @@ public class PlayerMovement : MonoBehaviour
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.velocity.y);
@@ -89,6 +169,9 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool("isRunning", hasHorizontalSpeed);
     }
 
+    /// <summary>
+    /// Flip the sprite
+    /// </summary>
     void FlipSprite(){
             bool hasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
             if(hasHorizontalSpeed){
@@ -97,6 +180,9 @@ public class PlayerMovement : MonoBehaviour
             }
     }
 
+    /// <summary>
+    /// Check if the player is on the ground
+    /// </summary>
     void GroundCheck(){
         isGrounded = false;
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, LayerMask.GetMask("Ground"));
@@ -105,19 +191,29 @@ public class PlayerMovement : MonoBehaviour
         }
         animator.SetBool("isJumping", !isGrounded);
     }
-
+    /// <summary>
+    /// Check if the player is touching a hazard and die
+    /// </summary>
     void DieByHazard(){
         if(capsule.IsTouchingLayers(LayerMask.GetMask("Hazards"))){
             capsule.GetComponent<Health>().PlayerTakeDamage(hazardDamage);
         }
     }
 
+    /// <summary>
+    ///  Play splash sound when player touches water
+    /// </summary>
+    /// <param name="other"></param>
     void OnTriggerEnter2D(Collider2D other) {
         if(capsule.IsTouchingLayers(LayerMask.GetMask("Water"))){
             audioPlayer.PlaySplashClip();
         }
     }
 
+    /// <summary>
+    /// Delay for the slash
+    /// </summary>
+    /// <returns></returns>
     IEnumerator SlashDelay(){
         yield return new WaitForSeconds(0.15f);
         Instantiate(slash, slashPoint.position, Quaternion.identity);
