1ce0d7398f9474b7cecd18e517a328285337db15
Tran Xuan Quang <quangtxhe153654@fpt.edu.vn>
2023-03-21 23:45:23 +0700

    comment function

--- Assets/Scripts/Slash.cs
+++ Assets/Scripts/Slash.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class Slash : MonoBehaviour
 {
-    Rigidbody2D slashRigid;
-    [SerializeField] float slashSpeed = 20f;
-    [SerializeField] int slashDamage = 20;
-    PlayerMovement player;
+    Rigidbody2D slashRigid; //Khoi tao rigid body 2D
+    [SerializeField] float slashSpeed = 20f; //Toc do bay cua slash
+    [SerializeField] int slashDamage = 20; //Dame moi phat chem
+    PlayerMovement player; //Khoi tao player movement
     float horizontalSlashSpeed;
 
     void Start()
     {
         slashRigid = GetComponent<Rigidbody2D>();
         player = FindObjectOfType<PlayerMovement>();
-        horizontalSlashSpeed = player.transform.localScale.x * slashSpeed;
+        horizontalSlashSpeed = player.transform.localScale.x * slashSpeed; //Khoang cach bay cua slash
     }
 
     void Update()
     {
-        slashRigid.velocity = new Vector2(horizontalSlashSpeed, 0f);
+        slashRigid.velocity = new Vector2(horizontalSlashSpeed, 0f); //van toc bay cua slash
     }
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Enemy"){
-            other.GetComponent<Health>().EnemyTakeDamage(slashDamage);
+            other.GetComponent<Health>().EnemyTakeDamage(slashDamage); //tru mau cua enemy
         }
         Destroy(gameObject);
     }
 
     void OnCollisionEnter2D(Collision2D other) {
-        Destroy(gameObject);
+        Destroy(gameObject);  //va cham nhau thi destroy slash
     }
 }
